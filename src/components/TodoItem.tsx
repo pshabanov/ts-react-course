33fe7f597c42b44794ed0f364ee0501c1968f203
Pavel Shabanov <pshabanov@nurtelecom.kg>
2023-09-26 12:34:10 +0600

    #0002(фича): Типизация стилей+типизация пропсов

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export interface TodoItemProps {
+	id: string
+	title: string
+	completed: boolean
+	style?: React.CSSProperties
+}
+
+const TodoItem = ({id, title, completed, style = {}}: TodoItemProps) => {
+	return (
+		<li style={ {color: 'red', backgroundColor: 'white', ...style} }>
+			<input type="checkbox" checked={ completed }/>
+			<span>{ title }</span>
+			<span>&times;</span>
+		</li>
+	)
+}
+
+export default TodoItem
